2b0415bf4e0738b8b5e883021dadad6ff0e41669
Portillo <gportillo@epiqsystems.com>
2018-07-11 10:00:07 -0500

    first test

---
+++ character.js
@@ -0,0 +1,8 @@
+class character {
+    
+    constructor(name = 'name') {
+        this.name = name;
+    }
+}
+
+module.exports = character;
--- test/character.test.js
+++ test/character.test.js
@@ -3,8 +3,13 @@ const character = require('../character');
 const name = 'Jeremy';
 
 describe('Character', () => {
-  it('it can get and set a name', () => {
-    const jeremy = new character;
+  it('it can get a name', () => {
+    const jeremy = new character(name);
+    assert.equal(jeremy.name, name);
+  });
+
+  it('can set a name', () => {
+    const jeremy = new character();
     jeremy.name = name;
     assert.equal(jeremy.name, name);
   });
