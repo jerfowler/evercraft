bbffa27b61d5f89a7aa0d079e050f3c2cb7b8c47
John Lane <jlane@sportinginnovations.com>
2018-07-11 11:15:24 -0500

    Add tests for abilities

--- test/character.test.js
+++ test/character.test.js
@@ -37,3 +37,29 @@ describe('Alignment', () => {
   });
 
 });
+
+describe('Abilities', () => {
+
+  it('can set abilities with a default of 10 for each', () => {
+    const sam = new character();
+    const abilities = ['strength', 'intelligence', 'wisdom', 'dexterity', 'constitution', 'charisma'];
+
+    abilities.forEach((ability) => {
+      assert.equal(sam.abilities[ability], 10);
+    });
+
+  });
+
+  it('cannot be set greater than 20', () => {
+    const sam = new character();
+    sam.abilities.strength = 21;
+    assert.equal(sam.abilities.strength, 10);
+  });
+
+  it('cannot be set less than 1', () => {
+      const sam = new character();
+      sam.abilities.strength = 0;
+      assert.equal(sam.abilities.strength, 10);
+  });
+
+});
