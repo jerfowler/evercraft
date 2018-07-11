22a9ccee208deb43be3729890424ee24229bd003
John Lane <jlane@sportinginnovations.com>
2018-07-11 10:21:24 -0500

    Add negative tests for alignment

--- test/character.test.js
+++ test/character.test.js
@@ -21,6 +21,19 @@ describe('Alignment', () => {
   it('can get and set alignment', () => {
     const john = new character();
     john.alignment = evil;
-    asert.equal(john.alignment, evil);
+    assert.equal(john.alignment, evil);
   });
+
+  it('won\'t accept poop', () => {
+    const john = new character();
+    john.alignment = 'poop';
+    assert.notEqual(john.alignment, 'poop');
+  });
+
+  it('defaults to Neutral', () => {
+      const john = new character();
+      john.alignment = 'poop';
+      assert.equal(john.alignment, 'Neutral');
+  });
+
 });
