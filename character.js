90e5a9df519dbbc86885388865a3b3d4ec4572f6
John Lane <jlane@sportinginnovations.com>
2018-07-11 10:19:49 -0500

    Add getter and setter for name

--- character.js
+++ character.js
@@ -1,7 +1,15 @@
 class character {
 
-    constructor(name = 'name') {
-        this.name = name;
+    constructor() {
+        this._name = 'name';
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(val) {
+        this._name = val;
     }
 
 
